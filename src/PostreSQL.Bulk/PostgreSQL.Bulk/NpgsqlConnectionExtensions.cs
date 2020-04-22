e85b24165e71d3ebddc4ecee66399cd64c87ab74
Twenty <twenty@translucent.at>
2020-04-23 01:07:29 +0200

    Removed unused methods

--- src/PostreSQL.Bulk/PostgreSQL.Bulk/NpgsqlConnectionExtensions.cs
+++ src/PostreSQL.Bulk/PostgreSQL.Bulk/NpgsqlConnectionExtensions.cs
@@ -50,43 +50,6 @@ namespace PostgreSQL.Bulk
             return await CompleteAndWriteRelationValues(connection, binaryImporter, entityDefinition, entities, cancellationToken);
         }
 
-        internal static Task<ulong> BulkInsertForeignAsync<TEntity, TTarget>(this NpgsqlConnection connection, IEnumerable<TEntity> entities, Func<IEnumerable<TEntity>, IEnumerable<TTarget>> flattenedForeignColumns, CancellationToken cancellationToken = default) where TEntity : class
-                                                                                                                                                                                                                                                                                                                    where TTarget : class
-        {
-            if (!EntityDefinitionCache.TryGetEntity<TTarget>(null, out var targetDefinition))
-            {
-                throw new TypeArgumentException($"The type {nameof(TTarget)} does not have a valid configuration already build and no was found in the current assembly.");
-            }
-
-            var copyStatement = CompileCopyStatement(targetDefinition!);
-
-            return PerformForeignCopy(connection, targetDefinition, entities, flattenedForeignColumns, copyStatement, cancellationToken);
-        }
-
-        private static async Task<ulong> PerformForeignCopy<TEntity, TTarget>(NpgsqlConnection connection, EntityDefinition<TTarget> entityDefinition, IEnumerable<TEntity> entities, Func<IEnumerable<TEntity>, IEnumerable<TTarget>> flattenedForeignColumns, string command, CancellationToken cancellationToken) where TEntity : class
-                                                                                                                                                                                                                                                                                                                                                           where TTarget : class
-        {
-            var binaryImporter = connection.BeginBinaryImport(command);
-
-            var foreignColumns = new HashSet<TTarget>();
-
-            foreach (var entity in flattenedForeignColumns.Invoke(entities))
-            {
-                await binaryImporter.StartRowAsync();
-
-                for (int entityColumnIndex = 0; entityColumnIndex < entityDefinition.ColumnDefinitions.Count; entityColumnIndex++)
-                {
-                    var entityColumn = entityDefinition.ColumnDefinitions[entityColumnIndex];
-
-                    await entityColumn.WriteValues(entity, binaryImporter, cancellationToken);
-                }
-
-                foreignColumns.Add(entity);
-            }
-
-            return await CompleteAndWriteRelationValues(connection, binaryImporter, entityDefinition, foreignColumns, cancellationToken);
-        }
-
         private static async Task<ulong> CompleteAndWriteRelationValues<TEntity>(NpgsqlConnection connection, NpgsqlBinaryImporter binaryImporter, EntityDefinition<TEntity> entityDefinition, IEnumerable<TEntity> entities, CancellationToken cancellationToken) where TEntity : class
         {
             ulong insertCount = await binaryImporter.CompleteAsync(cancellationToken);
